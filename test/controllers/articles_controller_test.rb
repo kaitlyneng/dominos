ae52a2ecd3230340190a5d073ece68a8aad81a77
kaitlyneng <kaitlyneng12@gmail.com>
2020-09-06 17:48:19 -0700

    Delete comments

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "should get index" do 
     get articles_url
